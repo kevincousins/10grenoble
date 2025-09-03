2af1105582e7202fc495723ab6e70cea3d885141
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-09-03 07:43:04 -0400

    initial git upload

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 11605395,
+    "boundingBox": {
+        "lx": -2.741974115371704,
+        "ly": -4.636917591094971,
+        "lz": -0.094,
+        "ux": 5.044025884628296,
+        "uy": 3.1490824089050299,
+        "uz": 7.692
+    },
+    "tightBoundingBox": {
+        "lx": -2.741974115371704,
+        "ly": -4.636917591094971,
+        "lz": -0.094,
+        "ux": 1.3450258846282957,
+        "uy": 3.1490824089050299,
+        "uz": 3.642
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.06742873787879944,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
